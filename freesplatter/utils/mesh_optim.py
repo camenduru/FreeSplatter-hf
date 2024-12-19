c7787734823b58b8d3f7804a2099e9961df3f10f
Jiale Xu <bluestyle97@users.noreply.huggingface.co>
2024-12-19 06:19:25 +0000

    Update freesplatter/utils/mesh_optim.py

--- freesplatter/utils/mesh_optim.py
+++ freesplatter/utils/mesh_optim.py
@@ -11,8 +11,6 @@ import cv2
 from PIL import Image
 import fast_simplification
 
-from freesplatter.utils.mesh import Mesh
-
 
 def parametrize_mesh(vertices: np.array, faces: np.array):
     """
@@ -153,7 +151,7 @@ def bake_texture(
 
 
 def optimize_mesh(
-    mesh: Mesh,
+    mesh,
     images: torch.Tensor,
     masks: torch.Tensor,
     extrinsics: torch.Tensor,
@@ -165,16 +163,16 @@ def optimize_mesh(
     """
     Convert a generated asset to a glb file.
     Args:
-        mesh (Mesh): Extracted mesh.
+        mesh (trimesh.Trimesh): Extracted mesh.
         simplify (float): Ratio of faces to remove in simplification.
         texture_size (int): Size of the texture.
         verbose (bool): Whether to print progress.
     """
-    vertices = mesh.v.cpu().numpy()
-    faces = mesh.f.cpu().numpy()
+    vertices = np.array(mesh.vertices).astype(float)
+    faces = np.array(mesh.faces).astype(int)
 
     # mesh simplification
-    max_faces = 50000
+    max_faces = 30000
     mesh_reduction = max(1 - max_faces / faces.shape[0], simplify)
     vertices, faces = fast_simplification.simplify(
         vertices, faces, target_reduction=mesh_reduction)
